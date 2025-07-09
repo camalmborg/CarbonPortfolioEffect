b72240b2b9b095d62900a81634a4120ee73eefb9
camalmborg <malmborg@bu.edu>
2025-07-09 17:22:02 -0400

    finished plot code for 2021 soc county level in California

--- 03_EDF_SD_vs_Area_Plots_script.R
+++ 03_EDF_SD_vs_Area_Plots_script.R
@@ -10,9 +10,14 @@ library(ggplot2)
 #files <- list.files(outputs_folder)[grep("shp",list.files(outputs_folder))]
 # vector:
 #vec <- vect(paste0(outputs_folder, "/", files[1]))
-#vec <- read.csv(paste0(outputs_folder,"/", files[1]))
 
-vec <- vec_list[[1]]
+# variable names for plots:
+var_names <- c("AGB", "SOC", "LAI", "SMF")
+
+# choose vector object with naive and ensemble data:
+var <- 2
+vec <- vec_list[[var]]
+plot_var_name <- var_names[var]
 
 ## SD vs aggregation area plots
 # find county areas in m^2:
@@ -44,18 +49,22 @@ plot_data <- as.data.frame(vec) %>%
 # color palette:
 plot_palette <- c("orchid4", "chocolate3")
 
-SD_vs_area <- ggplot(plot_data, aes(county_area_m2, value, color = variable)) +
+SD_vs_area <- ggplot(plot_data, aes(county_area_m2, value, color = variable, fill = variable)) +
   geom_point() +
-  ggtitle("Naive vs. Ensemble SD calculations: AGB") +
+  geom_smooth(method = "lm", se = TRUE, size = 0.5, alpha = 0.15) +
+  ggtitle(paste0("Naive vs. Ensemble SD calculations: ", plot_var_name)) +
   labs(x = "Area (square meters)",
        y = "SD", 
-       color = "SD Calculation") +
+       color = "SD Calculation",
+       fill = "SD Calculation") +
   scale_color_manual(values = plot_palette, 
                      labels = c("Ensemble", "Naive")) +
+  scale_fill_manual(values = plot_palette, 
+                    labels = c("Ensemble", "Naive")) +
   theme_bw()
 # view:
 SD_vs_area
 
-plot(vec$crops_area_m2, vec$crop_ensVar_SD, pch = 16, col = "blue",
-     xlab = "total crop area per county (m^2)", ylab = "SD")
-points(vec$crops_area_m2, vec$crop_Tot_SD, pch = 16, col = "red")
+# plot(vec$crops_area_m2, vec$crop_ensVar_SD, pch = 16, col = "blue",
+#      xlab = "total crop area per county (m^2)", ylab = "SD")
+# points(vec$crops_area_m2, vec$crop_Tot_SD, pch = 16, col = "red")
