cb72091d7af9d20185c6e1e7ea4f76902c19ece3
camalmborg <malmborg@bu.edu>
2025-07-29 11:37:59 -0400

    figuring out how to get croplands extent for midwest region

--- 05_EDF_Midwest_States_Expansion_script.R
+++ 05_EDF_Midwest_States_Expansion_script.R
@@ -16,6 +16,29 @@ source("/projectnb/dietzelab/malmborg/EDF_C_Portfolio_Project/02_EDF_SDA_Compari
 dir <- "/projectnb/dietzelab/malmborg/EDF/"
 setwd(dir)
 
+# load midwestern croplands from USGS 30m croplands extent layer:
+rast_crops <- rast("rasters/midwest_croplands.tif")
+rast_crops <- project(rast_crops, "EPSG:3857")
+
+# testing tigris:
+cornbelt <- vect(tigris::counties(c("Illinois", "Indiana", "Iowa"))) # Note FIPS codes for states: 17 = Illinois, 18 = Indiana, 19 = Iowa
+# reproject:
+cornbelt <- terra::project(cornbelt, rast_crops)
+
+# crop the raster to study area:
+rast_crops <- terra::crop(rast_crops, cornbelt)
+# apply mask:
+rast_crops_mask <- terra::mask(rast_crops, cornbelt)
+# make a croplands area raster:
+rast_crops_mask[rast_crops_mask != 2] <- NA
+rast_crops_mask[rast_crops_mask == 2] <- 1
+# save for later:
+writeRaster(rast_crops_mask, "rasters/midwest_crops_classed.tif")
+
+# test is_crop function:
+test <- get_is_crop(rast_crops_mask)
+
+# ### Merging croplans raster tiles for Midwest region
 # # load raster for crops:
 # # note: 2 = cropland areas
 # rasters <- list.files(paste0(dir, "rasters/"))
@@ -32,19 +55,10 @@ setwd(dir)
 # #writeRaster(rast_merge, "rasters/midwest_croplands.tif")
 # rm(rast, rast_collection, rast_list, rast_merge)
 
-# load midwestern croplands from USGS 30m croplands extent layer:
-rast_crops <- rast("rasters/midwest_croplands.tif")
-rast_crops <- project(rast_crops, "EPSG:3857")
-
+# ### Alternative to tigris county vectors
 # # loading vector of counties:
 # us_counties <- terra::vect("shapefiles/tl_2024_us_county.shp")
 # # reproject to match croplands raster:
 # us_counties <- terra::project(us_counties, rast_crops)
 # # separate counties for midwestern states Illinois, Indiana, Iowa:
 # cornbelt <- us_counties[us_counties$STATEFP %in% c("17", "18", "19")]
-
-# testing tigris:
-cornbelt <- vect(tigris::counties(c("Illinois", "Indiana", "Iowa"))) # Note FIPS codes for states: 17 = Illinois, 18 = Indiana, 19 = Iowa
-# reproject:
-cornbelt <- terra::project(cornbelt, rast_crops)
-
