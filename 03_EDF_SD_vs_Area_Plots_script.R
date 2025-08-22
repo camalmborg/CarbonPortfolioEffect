7e79eaf90be4677a8fc2a03314ef104f2c2f5c77
camalmborg <malmborg@bu.edu>
2025-08-22 15:37:06 -0400

    prepping for automating plot-making

--- 03_EDF_SD_vs_Area_Plots_script.R
+++ 03_EDF_SD_vs_Area_Plots_script.R
@@ -5,13 +5,6 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-# read in a vector:
-#outputs_folder <- '/projectnb/dietzelab/malmborg/EDF/SDA_Uncert_Outputs/'
-#files <- list.files(outputs_folder)[grep("shp",list.files(outputs_folder))]
-#load("/projectnb/dietzelab/malmborg/EDF/R_Environments/2025_07_21_environment.RData")
-# vector:
-#vec <- vect(paste0(outputs_folder, "/", files[1]))
-
 # variable names for plots:
 var_names <- c("AGB", "SOC", "LAI", "SMF")
 
@@ -21,35 +14,39 @@ var_names <- c("AGB", "SOC", "LAI", "SMF")
 cty <- mw_county#[[var]]
 #twn <- twnshp[[var]]
 #hyd <- hydr_reg[[var]]
-
-plot_var_name <- var_names[2]
+twn <- mw_towns
 
 ## SD vs aggregation area plots
 # find county areas in m^2:
 #vec$county_area_m2 <- terra::expanse(vec, unit = "m")
 cty$area_m2 <- terra::expanse(cty, unit = "m")
-#twn$area_m2 <- terra::expanse(twn, unit = "m")
+twn$area_m2 <- terra::expanse(twn, unit = "m")
 
 # add what each is:
 cty$type <- "county"
-#twn$type <- "township"
+twn$type <- "township"
 
 # reclass is_crop raster object to prepare for convert to polygon:
+is_crop <- get_is_crop(mw_rast_crops)
 reclass_iscrop <- as.factor(is_crop)
 reclass_iscrop <- ifel(is_crop == 1, 1, NA)
 # convert to polygons
 rast_poly <- as.polygons(reclass_iscrop)
 
 # find overlap between crop polygons and counties:
+#test <- get_is_crop(mw_rast_crops)
 #overlap <- terra::intersect(vec, rast_poly)
 overlap <- terra::intersect(cty, rast_poly)
-#overlap <- terra::intersect(twn, rast_poly)
+overlap <- terra::intersect(twn, rast_poly)
 # calculate overlap areas:
 cty$crops_area_m2 <- terra::expanse(overlap, unit = "m")
 #twn$crops_area_m2 <- terra::expanse(overlap, unit = "m")
 
 same_cols <- intersect(names(cty), names(twn))
 vec <- rbind(cty[same_cols], twn[same_cols])
+
+
+plot_var_name <- var_names[2]
 
 ### Plot crop area by county vs SD:
 # coerce to data.frame for plot:
@@ -84,7 +81,7 @@ SD_vs_area <- ggplot(plot_data, aes(area_m2, value, color = variable, fill = var
 # view:
 SD_vs_area
 
-plot(cty$crops_area_m2, cty$crop_ensVar_SD, pch = 16, col = "blue",
-     xlab = "total crop area per county (m^2)", ylab = "SD")
-points(cty$crops_area_m2, cty$crop_Tot_SD, pch = 16, col = "red")
+# plot(cty$crops_area_m2, cty$crop_ensVar_SD, pch = 16, col = "blue",
+#      xlab = "total crop area per county (m^2)", ylab = "SD")
+# points(cty$crops_area_m2, cty$crop_Tot_SD, pch = 16, col = "red")
 
