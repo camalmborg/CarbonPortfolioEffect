7d0b63a38d39237aba439eb31fe3500df7225429
camalmborg <malmborg@bu.edu>
2025-08-28 14:45:59 -0400

    script for doing whole CA run

---
+++ 06_EDF_California_Full_Run_script.R
@@ -0,0 +1,33 @@
+### CALIFORNIA RUN ###
+
+# run the California processing script:
+source("/projectnb/dietzelab/malmborg/EDF_C_Portfolio_Project/05_EDF_California_aggregates_script.R")
+
+# run the plotting script:
+source("/projectnb/dietzelab/malmborg/EDF_C_Portfolio_Project/03_EDF_SD_vs_Area_Plots_script.R")
+
+
+# save the plots and maps:
+save_dir <- "/projectnb/dietzelab/malmborg/EDF/Figures/"
+
+# Save the plot to a PNG file:
+ggsave(paste0(save_dir, "Plots/", Sys.Date(), "_CA_aggregate_regions_plot.png"),
+       plot = SD_vs_area,
+       dpi = 600)
+
+# Save the maps to PNG files:
+ggsave(paste0(save_dir, "Maps/", Sys.Date(), "_CA_town_maps.png"), 
+       plot = town_maps,
+       dpi = 600)
+
+ggsave(paste0(save_dir, "Maps/", Sys.Date(), "_CA_county_maps.png"), 
+       plot = county_maps,
+       dpi = 600)
+
+ggsave(paste0(save_dir, "Maps/", Sys.Date(), "_CA_region_maps.png"), 
+       plot = region_maps,
+       dpi = 600)
+
+ggsave(paste0(save_dir, "Maps/", Sys.Date(), "_CA_state_maps.png"), 
+       plot = state_maps,
+       dpi = 600)
