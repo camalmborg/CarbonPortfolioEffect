290ee6d3246660e18f37a4251f5077129727a501
camalmborg <malmborg@bu.edu>
2025-09-02 16:20:17 -0400

    changes to maps to make them look like California is attached to the US

--- 04_EDF_SD_Maps_script.R
+++ 04_EDF_SD_Maps_script.R
@@ -8,12 +8,20 @@ library(patchwork)
 
 # variable names for plots:
 var_names <- c("AGB", "SOC", "LAI", "SMF")
+# states for additional background:
+us_states <- tigris::states(cb = TRUE)
 
 # map function:
 #'@param var = numeric, number of variable name object you want - 2 for SOC
 #'@param vec = regional vector from ensemble vs naive processing step - e.g. _county, town, etc
 make_region_maps <- function(var, vec){
+  # name plot by variable
   plot_var_name <- var_names[var]
+  
+  # states for additional background:
+  us_states <- vect(tigris::states(cb = TRUE))
+  us_states <- project(us_states, vec)
+  us_sf <- st_as_sf(us_states)
   
   # when I have to make plots
   map_palette = c('#fff7ec','#fee8c8','#fdd49e','#fdbb84','#fc8d59','#ef6548','#d7301f','#b30000','#7f0000')
@@ -44,26 +52,41 @@ make_region_maps <- function(var, vec){
     # Convert back to a character vector
     as.character()
   
+  # get proper extent:
+  ext <- ext(sf_vec)
+  
   # plot naive:
   map1 <- ggplot(sf_vec) +
+    geom_sf(data = us_sf) +
     geom_sf(aes(fill = naive_bin)) +
-    geom_sf(data = sf_vec, fill = NA, color = "black", size = 0.4) +
+    geom_sf(data = sf_vec, fill = NA, color = "black", size = 0.3) +
     labs(title = paste0("Naive vs. Ensemble SD calculations: ", plot_var_name)) +
     scale_fill_manual(
       values = map_palette, name = "Naive SD") +
+    coord_sf(xlim <- c(ext[1], ext[2]), 
+             ylim <- c(ext[3], ext[4])) +
     theme_minimal() +
-    theme(legend.position = "none")
+    theme(legend.position = "none",
+          panel.background = element_rect(fill = "lightblue", color = NA),
+          panel.grid.major = element_line(color = "white", linewidth = 0.25, linetype = "dashed"),  # grid lines
+          panel.grid.minor = element_line(color = "white", linewidth = 0.25, linetype = "dashed"))
   
   # plot ensemble:
   map2 <- ggplot(sf_vec) +
+    geom_sf(data = us_sf) +
     geom_sf(aes(fill = ens_bin)) +
     geom_sf(data = sf_vec, fill = NA, color = "black", size = 0.4) +
-    #labs(title = paste0("Naive vs. Ensemble SD calculations: ", plot_var_name)) +
     scale_fill_manual(
       values = map_palette, 
       name = "Aggregated SD",
       labels = labels) +
-    theme_minimal()
+    coord_sf(xlim <- c(ext[1], ext[2]), 
+             ylim <- c(ext[3], ext[4])) +
+    theme_minimal() +
+    theme(legend.position = "none",
+          panel.background = element_rect(fill = "lightblue", color = NA),
+          panel.grid.major = element_line(color = "white", linewidth = 0.25, linetype = "dashed"),  # grid lines
+          panel.grid.minor = element_line(color = "white", linewidth = 0.25, linetype = "dashed"))
   
   # make together:
   compare_maps <- map1 + map2 + plot_layout(guides = "collect")
