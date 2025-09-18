6f1d8315c8922aef45fb89da478e13e0fcb8819e
camalmborg <malmborg@bu.edu>
2025-09-18 10:57:12 -0400

    function for naive uncertainty calculation for portfolios

--- 01_EDF_SDA_Portfolio_Sampling_functions_script.R
+++ 01_EDF_SDA_Portfolio_Sampling_functions_script.R
@@ -33,5 +33,43 @@ portfolio_sampler <- function(crop_group, ens_rast, n_pixels){
   portfolio_crop <- crop(crop_mask, portfolio_sample)
   # mask to portfolio:
   portfolio_mask <- mask(portfolio_crop, portfolio_sample)
-  return(portfolio_mask)
+  # make a vector:
+  portfolio_poly <- as.polygons(portfolio_mask)
+  # turn into individual polygons
+  portfolio_poly <- disagg(portfolio_poly)
+  # project to ens_rast:
+  portfolio_poly <- project(portfolio_poly, ens_rast[[1]])
+  # return the portfolio:
+  return(portfolio_poly)
 }
+
+## 2. Function for naive uncertainty for portfolio:
+#'@param ens_rast = ens_rast object of processed SDA outputs
+#'@param portfolio_sample = portfolio vector object created with portfolio_sampler function
+portfolio_naive <- function(ens_rast, portfolio_sample){
+  # separate mean/std tiffs and ensemble members:
+  ens_mean <- ens_rast[[names(ens_rast)[grep("mean", names(ens_rast))]]]
+  ens_std <- ens_rast[[names(ens_rast)[grep("std", names(ens_rast))]]]
+  
+  # Calculate by aggregating region:
+  RegORIG <- portfolio_sample
+  Reg <- terra::project(RegORIG, ens_mean)
+  
+  # means for portfolio:
+  RegCropMean <- terra::extract(ens_mean, Reg, fun = mean, na.rm = TRUE)
+  Reg[["cropMean"]] <- RegCropMean$mean
+  
+  # totals for area that is just cropland:
+  RegCropTotMean <- terra::extract(ens_mean, Reg, fun = sum, na.rm = TRUE) 
+  Reg[["cropTot"]] <- RegCropTotMean$mean
+  
+  # naive uncertainty:
+  cropVar <- ens_std^2
+  RegCropTotVar = terra::extract(cropVar, Reg, fun = sum, na.rm = TRUE)
+  Reg[["crop_Tot_CV"]] <- sqrt(RegCropTotVar[2])/RegCropTotMean$mean
+  Reg[["crop_Tot_SD"]] <- sqrt(RegCropTotVar[2])
+  
+  # return portfolio vector with naive calc:
+  return(Reg)
+}
+  
