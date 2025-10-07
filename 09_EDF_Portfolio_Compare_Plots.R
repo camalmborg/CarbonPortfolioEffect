ebb7fbf76e71ee46cd2096d99c08411f475dbd1f
camalmborg <malmborg@bu.edu>
2025-10-07 16:14:36 -0400

    fixing a whole bunch of problem

--- 09_EDF_Portfolio_Compare_Plots.R
+++ 09_EDF_Portfolio_Compare_Plots.R
@@ -12,11 +12,3 @@ dir <- "/projectnb/dietzelab/malmborg/EDF/"
 setwd(dir)
 # portfolio files:
 port_files <- list.files(paste0(dir, "CA_MW_portfolio_runs/Portfolios/"))
-
-# make a big data frame of all portfolios of all crops:
-## load crop portfolio
-load(paste0(dir, "CA_MW_portfolio_runs/Portfolios/", port_files[1]))
-## make it a data frame
-cp_list <- lapply(crop_portfolios, as.data.frame)
-cp <- do.call(rbind, cp_list)
-## bind all portfolios
