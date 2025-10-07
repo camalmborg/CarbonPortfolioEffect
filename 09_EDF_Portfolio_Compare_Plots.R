b3d4122efb49a8fe111052da92f035be76f44c82
camalmborg <malmborg@bu.edu>
2025-10-07 12:51:46 -0400

    loading port_files

--- 09_EDF_Portfolio_Compare_Plots.R
+++ 09_EDF_Portfolio_Compare_Plots.R
@@ -15,6 +15,7 @@ port_files <- list.files(paste0(dir, "CA_MW_portfolio_runs/Portfolios/"))
 
 # make a big data frame of all portfolios of all crops:
 ## load crop portfolio
+load(paste0(dir, "CA_MW_portfolio_runs/Portfolios/", port_files[1]))
 ## make it a data frame
 cp_list <- lapply(crop_portfolios, as.data.frame)
 cp <- do.call(rbind, cp_list)
