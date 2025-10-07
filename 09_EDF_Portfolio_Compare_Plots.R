56fbdfcd1f2c8c615d870669cf58e4c47d054440
camalmborg <malmborg@bu.edu>
2025-10-07 12:50:48 -0400

    first commit preparing ratio plot for all portfolios

---
+++ 09_EDF_Portfolio_Compare_Plots.R
@@ -0,0 +1,21 @@
+### Script for The Big Ratio Regression ###
+
+## Load libraries
+library(dplyr)
+library(tidyverse)
+library(sf)
+library(terra)
+
+## Load crop portfolios
+# set working directory:
+dir <- "/projectnb/dietzelab/malmborg/EDF/"
+setwd(dir)
+# portfolio files:
+port_files <- list.files(paste0(dir, "CA_MW_portfolio_runs/Portfolios/"))
+
+# make a big data frame of all portfolios of all crops:
+## load crop portfolio
+## make it a data frame
+cp_list <- lapply(crop_portfolios, as.data.frame)
+cp <- do.call(rbind, cp_list)
+## bind all portfolios
