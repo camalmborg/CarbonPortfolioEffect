f811201b2dbe541ccb744841b21fa7bf51ee2c3b
camalmborg <malmborg@bu.edu>
2025-10-07 17:22:39 -0400

    turns out it is still not great

--- 09_EDF_Portfolio_Compare_Plots.R
+++ 09_EDF_Portfolio_Compare_Plots.R
@@ -8,7 +8,9 @@ library(terra)
 
 ## Load crop portfolios
 # set working directory:
-dir <- "/projectnb/dietzelab/malmborg/EDF/"
+dir <- "/projectnb/dietzelab/malmborg/EDF/CA_MW_portfolio_runs/Portfolios/"
 setwd(dir)
 # portfolio files:
-port_files <- list.files(paste0(dir, "CA_MW_portfolio_runs/Portfolios/"))
+port_files <- list.files(dir)
+# open and rbind
+test <- read.csv(port_files[1])
