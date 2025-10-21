60899e66434d9e1df00e9882e0f7c2139bfb33c0
camalmborg <malmborg@bu.edu>
2025-10-21 11:55:15 -0400

    making change over time maps

--- 00_EDF_Change_Over_Time_Maps_script.R
+++ 00_EDF_Change_Over_Time_Maps_script.R
@@ -1,13 +1,75 @@
 ### Script for making change over time maps ###
 
-## Prepare workspace
+## Prepare workspace ##
 # load libraries:
 library(dplyr)
 library(terra)
 library(sf)
-# directory:
+# set working directory:
+wd <- "/projectnb/dietzelab/malmborg/EDF/"
+setwd(wd)
 
+## Loading Files ##
+# navigate to Dongchen's North America runs:
+ens <- "/projectnb/dietzelab/dongchen/anchorSites/NA_runs/"
+# choose run:
+run <- "SDA_8k_site/downscale_maps_analysis_lc_ts_noGEDI_debias_rf/"
 
-# open each file for year 1 and year 2
+## Preparing for running selected variable and year across aggregate regions:
+# C variable:
+soc <- "TotSoilCarb_"
+
+# choose analysis run variables:
+var <- soc
+yr_one <- 2019
+yr_two <- 2024
+dir <- paste0(ens, run)
+# for saving: (to be compatible with original functions and Dongchen's NA product directory naming conventions)
+save_dir <- paste0(getwd(), "/", var, as.character(yr_two), "/")
+
+## Functions
+# (1) for loading rasters:
+load_rast <- function(dir, var, yr, nfile){
+  year <- paste0(dir, var, as.character(yr))
+  file <- list.files(year)[grep(nfile, list.files(year))]
+  tiff <- paste0(year, "/", file)
+  rast <- terra::rast(tiff)
+  return(rast)
+}
+
+## Make new GeoTIFF files with Y2-Y1 maps
+# MEAN:
+# load maps:
+old <- load_rast(dir, var, yr_one, "mean")
+new <- load_rast(dir, var, yr_two, "mean")
+# cellwise subtraction:
+delta <- new - old
+# save:
+writeRaster(delta, file = paste0(save_dir, var, "mean_diff_", yr_one, "_", yr_two, ".tiff"))
+
+# SD:
+# load maps:
+old <- load_rast(dir, var, 2019, "std")
+new <- load_rast(dir, var, 2024, "std")
+# cellwise difference between sd's:
+diff_sd <- sqrt(old^2 + new^2)
+# save:
+writeRaster(delta, file = paste0(save_dir, var, "std_diff_", yr_one, "_", yr_two, ".tiff"))
+
+#ENSEMBLES:
+n_ens = 100
+for (i in 1:n_ens){
+  print(i)
+  # get ensemble number for grabbing file:
+  ens <- paste0("ensemble_", as.character(i))
+  # load maps:
+  old <- load_rast(dir, var, 2019, ens)
+  new <- load_rast(dir, var, yr_two, ens)
+  # cellwise subtraction:
+  delta <- new - old
+  # save:
+  writeRaster(delta, file = paste0(save_dir, var, ens,"_diff_", yr_one, "_", yr_two, ".tiff"))
+}
+
 # subtract year 2 from year 1 to get the change over time
 # save as a new geotiff 
