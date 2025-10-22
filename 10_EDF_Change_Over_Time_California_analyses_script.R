b85baf40e7d5148016513392d7195af63059d8be
camalmborg <malmborg@bu.edu>
2025-10-22 16:23:15 -0400

    first commit - change over time script for California croplands

---
+++ 10_EDF_Change_Over_Time_California_analyses_script.R
@@ -0,0 +1,43 @@
+### Script for running change over time analyses ###
+
+## Load libraries
+library(dplyr)
+library(terra)
+library(sf)
+
+## Load functions and data
+# functions for portfolio processing and analysis:
+source("01_EDF_SDA_Comparing_Ensemble_Uncertainties_functions_script.R")
+source("01_EDF_SDA_Portfolio_Sampling_functions_script.R")
+
+# set working directory:
+wd <- "/projectnb/dietzelab/malmborg/EDF/"
+setwd(wd)
+
+## Load Data:
+# navigate to change over time maps:
+cot <- "Change_Over_Time/"
+# years of run:
+yr_one = 2019
+yr_two = 2024
+
+# load raster cell sizes:
+cell_size <- terra::rast("/projectnb/dietzelab/dongchen/anchorSites/NA_runs/SDA_8k_site/cell_size.tif")
+# load crops:
+crops <- vect("/projectnb/dietzelab/dietze/CARB/i15_Crop_Mapping_2021_SHP/i15_Crop_Mapping_2021.shp")
+
+
+## Process change over time product as with individual years:
+# C variables:
+soc <- "TotSoilCarb_"
+
+# choose analysis run variables:
+var <- soc
+dir <- cot
+ens_rast <- process_ensemble_members(dir = dir,
+                                     var = var,
+                                     year = yr_two,
+                                     crops = crops,
+                                     cell_size = cell_size)
+
+
