1f68124e75473fe00a23d824e827520ef58be0ca
camalmborg <malmborg@bu.edu>
2026-05-13 13:50:47 -0400

    loop for combining supplement figures

--- 00_combine_and_format_plots.R
+++ 00_combine_and_format_plots.R
@@ -45,6 +45,7 @@ dev.off()
 
 
 ## Loop for combining the portfolio and COT plots by crop type:
+# Start with Portfolios
 # open correct directories:
 port_dir <- "/projectnb/dietzelab/malmborg/EDF/Figures/Portfolio_Plots/"
 port_list <- list.files(port_dir)[grepl(".rds", list.files(port_dir)) & grepl("SD_vs", list.files(port_dir))]
@@ -64,7 +65,59 @@ for (i in 1:length(port_list)){
     loc <- "Midwest"
   }
   # load plot and change title:
-  plot <- readRDS(paste0(port_dir, name)) + labs(title = paste0(loc, " - ", crop))
+  plot <- readRDS(paste0(port_dir, name)) + labs(title = paste0(loc, " - ", crop, " Portfolio (2021)"))
   # save the plot in the list under the crop name:
-  port_list[[crop]] <- plot
+  port_plots[[crop]] <- plot
 }
+
+# Repeat for COT:
+# open correct directories:
+cot_dir <- "/projectnb/dietzelab/malmborg/EDF/Figures/Change_Over_Time_Plots/"
+cot_list <- list.files(cot_dir)[grepl(".rds", list.files(cot_dir)) & grepl("SD_vs", list.files(cot_dir))]
+
+# loop for making plots:
+cot_plots <- list()
+for (i in 1:length(cot_list)){
+  # load plot file name:
+  name <- cot_list[i] 
+  # extract crop type:
+  crop <- str_to_title(str_extract(name, "(?<=portfolio_).*?(?=_SD)"))
+  # extract location for plot title:
+  loc <- str_extract(name, "(?<=COT_).*?(?=_portfolio)")
+  if(loc == "CA"){
+    loc <- "California"
+  } else {
+    loc <- "Midwest"
+  }
+  # load plot and change title:
+  plot <- readRDS(paste0(cot_dir, name)) + labs(title = paste0(loc, " - ", crop, " Change Over Time (2019-2024)"))
+  # save the plot in the list under the crop name:
+  cot_plots[[crop]] <- plot
+}
+
+# loop for combining and saving plots:
+crops <- names(port_plots)
+for (i in 1:length(crops)){
+  # load matching plots:
+  port <- port_plots[[crops[i]]] + theme(axis.title = element_blank(), axis.text.x = element_blank()) 
+  cot <- cot_plots[[crops[i]]] + theme(legend.position = c(0.85, 0.15), legend.background = element_rect(color = "black", linewidth = 0.25))
+  
+  # combine plots:
+  crop_combine <- (port / cot) +
+    plot_layout(guides = "collect")
+  
+  # name plots:
+  name <- crops[i]
+  
+  # save the plots:
+  save_dir <- "/projectnb/dietzelab/malmborg/EDF/Figures/Z_final_paper_figures/Supplements/"
+  png(filename = paste0(save_dir, Sys.Date(), "_", name,"_SD_plots_CA_MW_combined.png"),
+      height = 10, width = 8, units = "in", res = 600)
+  print(crop_combine)
+  dev.off()
+  
+  # print progress:
+  print(name)
+}
+
+# end
